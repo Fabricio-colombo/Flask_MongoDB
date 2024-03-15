160fb6df294b157ad383a948651c0135af5b1167
Fabricio Colombo <fabriciocolombo0096@gmail.com>
2024-03-15 16:45:30 -0300

    web

--- projeto/app.py
+++ projeto/app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return 'Página de registro ainda não implementada.'
+
+@app.route('/forgot-password')
+def forgot_password():
+    return 'Página de esqueceu sua senha ainda não implementada.'
+
 if __name__ == '__main__':
     app.run(debug=True)
