76cbd6620f4ed984960398b4e085bfde58fa10b0
Fabricio Colombo <fabriciocolombo0096@gmail.com>
2024-03-19 10:23:03 -0300

    save1111

--- projeto/app.py
+++ projeto/app.py
@@ -1,27 +1,60 @@
-from flask import Flask, render_template
-from connect_mongo import connect_mongo
+from flask import Flask, render_template, redirect, url_for, request, session
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-collection = connect_mongo()
-if collection is not None:
-    print("Conexão bem-sucedida!")
-else:
-    print("Falha ao conectar")
+# Configurações do MongoDB
+URI_config = "mongodb://localhost:27017"
+nome_config = "Site_Flask"
+collection_name_config = "BD_LOGIN"
+
+# Chave secreta para a sessão (você pode gerar uma chave mais segura)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+# Conexão ao MongoDB
+client = MongoClient(URI_config)
+db = client[nome_config]
+collection = db[collection_name_config]
 
 @app.route('/')
 def index():
-    # http://127.0.0.1:5000/
+    if 'username' in session:
+        return render_template('index.html', username=session['username'])
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    # http://127.0.0.1:5000/login
-    return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+    elif request.method == 'GET':
+        username = request.args.get('username')
+        password = request.args.get('password')
 
-@app.route('/register')
+    user_data = collection.find_one({'username': username, 'password': password})
+    if user_data:
+        session['username'] = username  # Definir a variável de sessão
+        return redirect(url_for('index'))
+    else:
+        return render_template('login.html', error='Credenciais inválidas. Tente novamente.')
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return 'Página de registro ainda não implementada.'
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if collection.find_one({'username': username}):
+            return render_template('register.html', error='Nome de usuário já existe. Tente outro.')
+        else:
+            collection.insert_one({'username': username, 'password': password})
+            return redirect(url_for('index'))  # Redirecionar para a página inicial após o registro
+    return render_template('register.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)  # Remover a variável de sessão
+    return redirect(url_for('index'))
 
 @app.route('/forgot-password')
 def forgot_password():
